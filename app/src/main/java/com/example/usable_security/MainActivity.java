4af1fb9a725227ee1a20e76309dcf5817d1fd77d
Eliana <elianaellati2002@gmail.com>
2024-01-04 09:22:58 +0200

    ba700

--- app/src/main/java/com/example/usable_security/MainActivity.java
+++ app/src/main/java/com/example/usable_security/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.usable_security;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
@@ -7,10 +9,22 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.Executor;
 
@@ -22,16 +36,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
+        EditText Password=findViewById(R.id.EditPassword);
+        Password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
         TextView loginLink = findViewById(R.id.signupLink);
         loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*Log.d("MainActivity", "This is a debug message");
+                FirebaseDatabase database =FirebaseDatabase.getInstance();
+                DatabaseReference myRef=database.getReference("message");
+                myRef.setValue("hellooooo,World")
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // Write was successful!
+                                Log.d("MainActivity", "This is a debug message");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Write failed
+                                Log.e(TAG, "Error writing data", e);
+                            }
+                        });*/
                 // Open the login page (replace LoginActivity.class with your actual login activity)
                 Intent intent = new Intent(MainActivity.this, SignInActivity.class);
                 startActivity(intent);
             }
         });
+       Button loginn=findViewById(R.id.buttonlogin);
+        loginn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText Username=findViewById(R.id.usernameEditText);
+                String username=Username.getText().toString();
+                String password=Password.getText().toString();
+                if(!username.isEmpty()&& !password.isEmpty()){
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference dataRef = database.getReference("Data");
+                    dataRef.orderByChild("Username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            boolean usernameFound = false;
+
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                usernameFound = true;
+
+                                String storedPassword = snapshot.child("Password").getValue(String.class);
+                                String storedusername=snapshot.child("Username").getValue(String.class);
+                                if ( storedPassword.equals(password) && storedusername.equals(storedusername)) {
+                                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                                    startActivity(intent);
+
+                                } else {
+                                    // Username found, but password does not match
+                                    Log.d("FirebaseData", "Password does not match");
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle errors if any
+                            Log.e("FirebaseData", "Error retrieving data", databaseError.toException());
+                        }
+                    });
+                }
+            }
+    });
+
 
 
     }
--- app/src/main/java/com/example/usable_security/SignInActivity.java
+++ app/src/main/java/com/example/usable_security/SignInActivity.java
@@ -2,8 +2,11 @@ package com.example.usable_security;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,15 +15,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 import java.util.concurrent.Executor;
 
 public class SignInActivity extends AppCompatActivity {
 
+    int flag =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView loginLink = findViewById(R.id.login);
+        EditText Password=findViewById(R.id.EditPassword);
+        Password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        Password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
         Button signup=findViewById(R.id.buttonsignup);
         loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,30 +50,66 @@ public class SignInActivity extends AppCompatActivity {
             biometricPrompt.authenticate(getPromptInfo());
         });
         signup.setOnClickListener(view -> {
-            Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
-            startActivity(intent);
+            EditText Name =findViewById(R.id.nameEditText);
+            EditText Username=findViewById(R.id.usernameEditText);
+            EditText Email=findViewById(R.id.emailEditText);
+            if(flag==2){
+              String name=Name.getText().toString();
+              String username=Username.getText().toString();
+              String email=Email.getText().toString();
+              String password=Password.getText().toString();
+          if(!name.isEmpty() && !username.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+          HashMap<String,Object> hashmap=new HashMap<>();
+          hashmap.put("Name",name);
+          hashmap.put("Username", username);
+          hashmap.put("Email",email);
+          hashmap.put("Password",password);
+          FirebaseDatabase database =FirebaseDatabase.getInstance();
+          DatabaseReference Ref= database.getReference("Data");;
+          String key=Ref.push().getKey();
+          hashmap.put("key",key);
+          Ref.child(key).setValue(hashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+              @Override
+              public void onComplete(@NonNull Task<Void> task) {
+                  Toast.makeText(SignInActivity.this,"Added",Toast.LENGTH_SHORT).show();
+
+
+              }
+          });
+          Name.getText().clear();
+          Email.getText().clear();
+          Password.getText().clear();
+          Username.getText().clear();
+          Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+          startActivity(intent);
+      }
+
+            }
         });
 
 
     }
     private BiometricPrompt getPrompt() {
-        Executor executor = ContextCompat.getMainExecutor(this);
 
+        Executor executor = ContextCompat.getMainExecutor(this);
         BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
             @Override
             public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
+                flag=1;
                 notifyUser(errString.toString());
             }
 
             @Override
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
+                flag=2;
                 notifyUser("Authentication Succeeded!");
             }
 
             @Override
             public void onAuthenticationFailed() {
+                flag=1;
                 super.onAuthenticationFailed();
                 notifyUser("Authentication Failed!");
             }
