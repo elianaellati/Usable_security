b58d6679e2a5c6c4b13547c3d93702667a6201b7
Eliana <elianaellati2002@gmail.com>
2024-01-17 19:03:43 +0200

    ba700

--- app/src/main/java/com/example/usable_security/details.java
+++ app/src/main/java/com/example/usable_security/details.java
@@ -396,16 +396,17 @@ public class details extends AppCompatActivity implements NavigationView.OnNavig
                     for (Map.Entry<String, tasks> entry : taskMap.entrySet()) {
                         Log.d("Taskkkkkkkk", task.getName());
 
+                        task.setName(edtName.getText().toString());
+                        task.setNote(edtNote.getText().toString());
                         if (entry.getValue().getName().compareToIgnoreCase(task.getName()) == 0) {
                             editForSharedContacts(user, task);
                             DatabaseReference userTasksRef = FirebaseDatabase.getInstance().getReference().child("Data").child(User.key).child("tasks");
                             userTasksRef.child(entry.getKey()).child("name").setValue(edtName.getText().toString());
-                            userTasksRef.child(entry.getKey()).child("note").setValue(edtNote.getText().toString());
+                            userTasksRef.child(entry.getKey()).child("note").setValue(task.getNote());
                             userTasksRef.child(entry.getKey()).child("reminder").setValue(task.getReminder());
                             userTasksRef.child(entry.getKey()).child("repeat").setValue(task.getRepeat());
                             userTasksRef.child(entry.getKey()).child("time").setValue(task.getTime());
-                            task.setName(edtName.getText().toString());
-                            task.setNote(edtNote.getText().toString());
+
                             User updateuser = null;
                             updateuser = gson.fromJson(userJson, User.class);
                             updateuser.editTask(entry.getKey(), task);
